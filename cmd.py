388306ca9466e9da42aa9fd1cd8474fc08347908
ckbessel <ckbessel@ucalgary.ca>
2018-03-15 20:46:55 +0000

    Initial commit

---
+++ cmd.py
@@ -0,0 +1,11 @@
+from subprocess import PIPE, Popen
+
+
+def cmdline(command):
+    process = Popen(
+        args=command,
+        stdout=PIPE,
+        shell=True
+    )
+    return process.communicate()[0]
+
---
+++ publish.py
@@ -0,0 +1,9 @@
+from cmd import cmdline as cmd
+import time
+
+cmd("git add --all && git commit -m 'Initial commit' && git push -u origin master")
+sleep(10)
+cmd("ckbessel")
+sleep(2)
+cmd("jad3curtis")
+
