fea74657a5f91a6c940f269f1c6aee2446a3b610
ckbessel <ckbessel@ucalgary.ca>
2018-03-15 20:49:15 +0000

    Initial commit

--- publish.py
+++ publish.py
@@ -2,8 +2,6 @@ from cmd import cmdline as cmd
 import time
 
 cmd("git add --all && git commit -m 'Initial commit' && git push -u origin master")
-time.sleep(10)
-cmd("ckbessel")
-time.sleep(2)
-cmd("jad3curtis")
+
+
 
